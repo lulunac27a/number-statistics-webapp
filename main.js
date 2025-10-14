552c4a8fbed2e3cdff603107a0e8b8efc853a2dc
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-10-14 18:14:37 -0500

    Initialize number statistics webapp

---
+++ main.js
@@ -0,0 +1,7 @@
+const numberInputText = document.getElementById('number-input');
+const addNumberButton = document.getElementById('add-number');
+const numbers = [];
+addNumberButton.addEventListener('click', () => {
+    numbers.push(numberInputText.value)
+}
+);
