7ae36691f5083dc0e46ec70dc0012bdd98a96993
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-10-14 18:17:35 -0500

    Format code

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const numberInputText = document.getElementById('number-input');
-const addNumberButton = document.getElementById('add-number');
+const numberInputText = document.getElementById("number-input");
+const addNumberButton = document.getElementById("add-number");
 const numbers = [];
-addNumberButton.addEventListener('click', () => {
-    numbers.push(numberInputText.value)
+addNumberButton.addEventListener("click", () => {
+  numbers.push(numberInputText.value);
 });
