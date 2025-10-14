72a5355c02ab9794365bb06f0aaa3f5a32930669
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-10-14 18:21:37 -0500

    Add display to list of numbers

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const numberInputText = document.getElementById("number-input");
 const addNumberButton = document.getElementById("add-number");
+const numbersListText = document.getElementById("numbers-list");
 const numbers = [];
 addNumberButton.addEventListener("click", () => {
     numbers.push(numberInputText.value);
+    numbersListText.innerText = numbers.join(", ");
 });
