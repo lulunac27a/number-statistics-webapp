44157314503275cb5c5f2008438cd38e56ae0b3d
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-10-14 18:18:51 -0500

    Add Prettier and format code

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ const numberInputText = document.getElementById("number-input");
 const addNumberButton = document.getElementById("add-number");
 const numbers = [];
 addNumberButton.addEventListener("click", () => {
-  numbers.push(numberInputText.value);
+    numbers.push(numberInputText.value);
 });
