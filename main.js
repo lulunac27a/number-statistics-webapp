c6e5bb2532983ec6bb352b79a4d9aaf45eb04b2f
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-10-16 11:40:40 -0500

    Add JavaScript comments

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-const numberInputText = document.getElementById("number-input");
-const addNumberButton = document.getElementById("add-number");
-const numbersListText = document.getElementById("numbers-list");
-const sortedNumbersListText = document.getElementById("sorted-numbers-list");
-const numberCountText = document.getElementById("number-count");
-const numbers = [];
+const numberInputText = document.getElementById("number-input"); //number input text
+const addNumberButton = document.getElementById("add-number"); //add number button
+const numbersListText = document.getElementById("numbers-list"); //numbers list text
+const sortedNumbersListText = document.getElementById("sorted-numbers-list"); //sorted numbers list text
+const numberCountText = document.getElementById("number-count"); //number count length text
+const numbers = []; //numbers list
 addNumberButton.addEventListener("click", () => {
-    numbers.push(parseFloat(numberInputText.value, 10));
-    const sortedNumbers = [...numbers].sort((a, b) => a - b);
-    numbersListText.innerText = numbers.join(", ");
-    sortedNumbersListText.innerText = sortedNumbers.join(", ");
-    numberCountText.innerText = numbers.length;
+    //when add number button is pressed
+    numbers.push(parseFloat(numberInputText.value, 10)); //parse entered numeric value as floating point number
+    const sortedNumbers = [...numbers].sort((a, b) => a - b); //sort numbers in ascending order
+    numbersListText.innerText = numbers.join(", "); //update numbers list text
+    sortedNumbersListText.innerText = sortedNumbers.join(", "); //update sorted numbers list text
+    numberCountText.innerText = numbers.length; //update number count length text
 });
