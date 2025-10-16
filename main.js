a6f0491d1d834c35907b5e7ea42af0a9528197bf
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-10-15 21:46:33 -0500

    Add output for length of numbers in array list

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ const numberInputText = document.getElementById("number-input");
 const addNumberButton = document.getElementById("add-number");
 const numbersListText = document.getElementById("numbers-list");
 const sortedNumbersListText = document.getElementById("sorted-numbers-list");
+const numberCountText = document.getElementById("number-count");
 const numbers = [];
 addNumberButton.addEventListener("click", () => {
     numbers.push(numberInputText.value);
     const sortedNumbers = [...numbers].sort((a, b) => a - b);
     numbersListText.innerText = numbers.join(", ");
     sortedNumbersListText.innerText = sortedNumbers.join(", ");
+    numberCountText.innerText = numbers.length;
 });
