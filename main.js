746c92c45fd658bd917501d6012ffe03bf03ba03
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-10-15 21:47:05 -0500

    Parse numeric input value as float

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const sortedNumbersListText = document.getElementById("sorted-numbers-list");
 const numberCountText = document.getElementById("number-count");
 const numbers = [];
 addNumberButton.addEventListener("click", () => {
-    numbers.push(numberInputText.value);
+    numbers.push(parseFloat(numberInputText.value));
     const sortedNumbers = [...numbers].sort((a, b) => a - b);
     numbersListText.innerText = numbers.join(", ");
     sortedNumbersListText.innerText = sortedNumbers.join(", ");
